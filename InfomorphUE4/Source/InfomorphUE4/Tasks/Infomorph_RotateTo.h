0ebddbf9912accecc09768d6fb77106afd77f8f4
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2017-06-17 21:31:33 +0200

    [*] Slightly better rotation code

--- InfomorphUE4/Source/InfomorphUE4/Tasks/Infomorph_RotateTo.h
+++ InfomorphUE4/Source/InfomorphUE4/Tasks/Infomorph_RotateTo.h
@@ -6,11 +6,6 @@
 #include "BehaviorTree/BTTaskNode.h"
 #include "Infomorph_RotateTo.generated.h"
 
-struct FRotateToMemory
-{
-	float LerpTime;
-};
-
 /**
  * 
  */
@@ -29,13 +24,12 @@ protected:
 private:
 	virtual FString GetStaticDescription() const override;
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-	virtual uint16 GetInstanceMemorySize() const override;
 
 protected:
 	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 
-	EBTNodeResult::Type PerformRotate(class AInfomorphUE4Character* ControlledActor, class AInfomorphUE4Character* TargetActor, float LerpT);
-	EBTNodeResult::Type PerformRotate(class AInfomorphUE4Character* ControlledActor, const FVector& TargetLocation, float LerpT);
+	EBTNodeResult::Type PerformRotate(class AInfomorphUE4Character* ControlledActor, class AInfomorphUE4Character* TargetActor, float DeltaSeconds);
+	EBTNodeResult::Type PerformRotate(class AInfomorphUE4Character* ControlledActor, const FVector& TargetLocation, float DeltaSeconds);
 
 public:
 	virtual void OnGameplayTaskActivated(class UGameplayTask& Task);
